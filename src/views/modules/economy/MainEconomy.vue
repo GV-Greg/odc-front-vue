2ac340d503e5f8bb0bbca0848eb31b6ad8b42827
Greg GV <gregvandenbergh@gmail.com>
2022-06-13 20:33:08 +0200

    Style modifications

--- src/views/modules/economy/MainEconomy.vue
+++ src/views/modules/economy/MainEconomy.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <main class="mx-5 mb-5 p-5 bg-gray-200 w-11/12 md:w-3/4 h-full flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-xl">
+  <main class="main md:w-3/4">
     <div class="overflow-y-auto flex flex-col flex-grow">
       <h2>Eco </h2>
     </div>
